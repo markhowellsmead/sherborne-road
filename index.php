4074c70f3be146876d1da3c6287cfcaf305118de
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-12-16 15:11:45 +0100

    Fix edit post link

--- index.php
+++ index.php
@@ -45,9 +45,13 @@
                             <header class="post-header">
                                 <h1 class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
                                 <?php if (get_post_type() !== 'page') : ?>
-                                    <time class="subheader"><?php printf(__('Published %s', 'sherborne_road'), get_the_time(get_option('date_format'))); ?></time>
+                                    <time class="subheader">
+                                        <?php
+                                        printf(__('Published %s', 'sherborne_road'), get_the_time(get_option('date_format')));
+                                        edit_post_link(__('Edit'), ' | ');
+                                        ?>
+                                    </time>
                                 <?php endif;?>
-                                <?php edit_post_link(__('Edit'), '<p>', '</p>');?>
                             </header>
                         <?php endif; ?>
 
