80ce95364a98377a1bb7b96dcff1c4c2b28d3fd3
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-02-25 17:34:21 +0100

    Improved photo sizing

--- template-parts/thumbnail.php
+++ template-parts/thumbnail.php
@@ -1,33 +1,33 @@
-<?php if (has_post_thumbnail()) : ?>
+<?php if (has_post_thumbnail()): ?>
 
     <div class="featured-image">
         <?php
-        if (is_singular()) {
-            the_post_thumbnail('post-thumbnail-full');
-        } else {
-            ?>
+if (is_singular()) {
+    the_post_thumbnail('photo-full');
+} else {
+    ?>
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
             <?php the_post_thumbnail('medium'); ?>
             </a>
         <?php
 
-        }
-        ?>
+}
+?>
     </div>
 
 <?php
 
-    elseif (!is_singular() && function_exists('sherborne_road_video_thumbnail') && $video_url = get_post_meta(get_the_ID(), 'video_ref', true)):
-        if ($thumbnail = sherborne_road_video_thumbnail($video_url)) {
-            printf(
-                '<div class="featured-image">
-                    <a href="%1$s" title="%2$s">
-                        <img src="%3$s">
-                    </a>
-                </div>',
-                get_the_permalink(),
-                get_the_title(),
-                $thumbnail
-            );
-        }
-    endif;
+elseif (!is_singular() && function_exists('sherborne_road_video_thumbnail') && $video_url = get_post_meta(get_the_ID(), 'video_ref', true)):
+    if ($thumbnail = sherborne_road_video_thumbnail($video_url)) {
+        printf(
+            '<div class="featured-image">
+	                    <a href="%1$s" title="%2$s">
+	                        <img src="%3$s">
+	                    </a>
+	                </div>',
+            get_the_permalink(),
+            get_the_title(),
+            $thumbnail
+        );
+    }
+endif;
