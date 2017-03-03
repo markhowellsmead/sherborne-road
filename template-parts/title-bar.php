9bcdfa73d84ad8f65ff3dd91e134a47eb3a5ccc2
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-03-03 13:47:44 +0100

    Foundation title-bar

--- template-parts/title-bar.php
+++ template-parts/title-bar.php
@@ -1,23 +1,25 @@
-<div class="title-bar sticky" data-sticky data-margin-top="0">
-    <div class="title-bar-inner">
-        <div class="title-bar-left">
-            <div class="hide-for-large">
-                <button class="menu-icon" type="button" data-toggle="offCanvas"></button>
-                <h2 class="title-bar-title"><a href="<?php echo esc_url(home_url()); ?>"><?php bloginfo('name'); ?></a></h2>
+<div data-sticky-container>
+    <div class="title-bar" data-sticky data-margin-top="0">
+        <div class="row align-justify align-bottom">
+            <div class="column">
+                <div class="row align-middle align-justify">
+                    <h2 class="title-bar-title"><a href="<?php echo esc_url(home_url()); ?>"><?php bloginfo('name'); ?></a></h2>
+                    <button class="hide-for-large menu-icon" type="button" data-toggle="offCanvas"></button>
+                </div>
             </div>
-        </div>
 
-        <div class="title-bar-right">
-            <?php
-                if (has_nav_menu('primary-menu')) {
-                    wp_nav_menu(array(
-                    'theme_location' => 'primary-menu',
-                    'items_wrap' => '<nav class="show-for-large"><ul class="menu">%3$s</ul></nav>',
-                    'fallback_cb' => false,
-                    'container' => false,
-                    ));
-                }
-            ?>
+            <div class="">
+                <?php
+                    if (has_nav_menu('primary-menu')) {
+                        wp_nav_menu(array(
+                        'theme_location' => 'primary-menu',
+                        'items_wrap' => '<nav class="show-for-large"><ul class="menu">%3$s</ul></nav>',
+                        'fallback_cb' => false,
+                        'container' => false,
+                        ));
+                    }
+                ?>
+            </div>
         </div>
     </div>
 </div>
