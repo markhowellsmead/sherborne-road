05a184455041424c064df8f8a06a31d6a3997c91
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-03-11 12:25:50 +0100

    Foundation - front page

--- template-parts/row-banner_image.php
+++ template-parts/row-banner_image.php
@@ -5,7 +5,7 @@ $out = array();
 
 $image = get_sub_field('image');
 
-$out[] = '<div class="module row banner_image '.$hero_size.' '.$overlay.' label-'.get_sub_field('label_position').'" style="background-image:url(\''.$image['sizes']['photo-full'].'\')">';
+$out[] = '<div class="module row banner_image expanded" style="background-image:url(\'' . $image['sizes']['photo-full'] . '\')">';
 $out[] = '</div>';
 
 echo implode('', $out);
