e1ba8600d003a3708956c581e35450c76ea84720
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-03-19 17:39:46 +0100

    Fix title bar and mobile navigation

--- template-parts/offcanvas.php
+++ template-parts/offcanvas.php
@@ -1,3 +1,3 @@
-<div class="off-canvas position-left is-transition-push" id="offCanvas" data-off-canvas>
-    hello world
+<div class="off-canvas position-left is-transition-push" id="offCanvas" data-off-canvas data-position="left">
+    <?php get_template_part('template-parts/navigation-mobile');?>
 </div>
