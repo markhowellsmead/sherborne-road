d397b725d874ac962afae3c5f00f4af715c13a55
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-03-24 18:33:01 +0100

    [BUGFIX] 404 content section layout

--- template-parts/404.php
+++ template-parts/404.php
@@ -1,5 +1,7 @@
-<div class="post">
-
-    <p><?php _e('Sorry, the page you requested cannot be found.', 'sherborne_road'); ?></p>
-
+<div class="row content-404 block-margin-after">
+	<div class="column">
+		<div class="post">
+		    <p><?php _e('Sorry, the page you requested cannot be found.', 'sherborne_road'); ?></p>
+		</div>
+	</div>
 </div>
