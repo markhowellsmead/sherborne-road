fae2d72b8d2ec009578edc0301e19ed335fe825a
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-03-28 13:31:21 +0200

    [TASK] Add buttons to hero element

--- template-parts/row-hero_element.php
+++ template-parts/row-hero_element.php
@@ -1,8 +1,5 @@
 <?php
 
-var_dump(have_rows('buttons'));
-exit;
-
 $image = get_sub_field('image');
 
 $srcset = array(
@@ -11,6 +8,28 @@ $srcset = array(
     $image['sizes']['photo-full'] .' ' .$image['sizes']['photo-full-width']. 'w',
     $image['url'] .' ' .$image['width']. 'w',
 );
+
+if (have_rows('buttons')) {
+    $buttons = '';
+    while (have_rows('buttons')) {
+        the_row();
+        switch (get_row_layout()) {
+            case 'primary_button':
+                $link = get_sub_field('button_link')['url'];
+                $buttons .= '<a href="' .$link.'" class="small button">'.get_sub_field('button_text').'</a>';
+                break;
+            case 'secondary_button':
+                $link = get_sub_field('button_link')['url'];
+                $buttons .= '<a href="' .$link.'" class="small button secondary">'.get_sub_field('button_text').'</a>';
+                break;
+        }
+    }
+    if ($buttons !== '') {
+        $buttons = '<div>'.$buttons.'</div>';
+    }
+} else {
+    $buttons = '';
+}
 
 printf(
     '<figure class="mod row expanded hero_element block-margin-after">
@@ -21,11 +40,12 @@ printf(
     implode(', ', $srcset),
     $image['alt'],
     !get_sub_field('title') ? '' : sprintf(
-        '<figcaption class="caption column small-12 medium-6"><h1>%1$s</h1>%2$s</figcaption>',
+        '<figcaption class="caption column small-12"><h1>%1$s</h1>%2$s%3$s</figcaption>',
         get_sub_field('title'),
         !get_sub_field('subtitle') ? '' : sprintf(
-            '<h2>%1$s</h2>',
+            '<p>%1$s</p>',
             get_sub_field('subtitle')
-        )
+        ),
+        $buttons
     )
 );
